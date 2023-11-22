3f6b6c4e508d5f63ac8ec36a650c2599be48ccee
Voqal <voqaldev@gmail.com>
2023-11-21 22:56:35 -0600

    modify method logic

--- examples/java/ModifyMethod.java
+++ examples/java/ModifyMethod.java
@@ -2,4 +2,16 @@ public class ModifyMethod {
     public int add(int x, int y) {
         return x + y;
     }
+
+    public int subtract(int x, int y) {
+        return x - y;
+    }
+
+    public static void main(String[] args) {
+        ModifyMethod calculator = new ModifyMethod();
+        int sum = calculator.add(5, 3);
+        int difference = calculator.subtract(10, 4);
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+    }
 }
