b532dc7c4148bb8952059447ed4e6d53b1b0dfd1
Brandon Fergerson <bfergerson@apache.org>
2024-09-03 16:28:37 -0300

    chore: clean

--- src/main/kotlin/dev/voqal/provider/clients/anthropic/AnthropicClient.kt
+++ src/main/kotlin/dev/voqal/provider/clients/anthropic/AnthropicClient.kt
@@ -56,104 +56,111 @@ class AnthropicClient(
 
     override suspend fun chatCompletion(request: ChatCompletionRequest, directive: VoqalDirective?): ChatCompletion {
         val log = project.getVoqalLogger(this::class)
-        try {
-            val maxTokens = 4096 //todo: configurable
-            val requestJson = JsonObject()
-                .put("model", request.model.id)
-                .put("messages", JsonArray(request.messages.map { it.toJson() }))
-                .put("max_tokens", maxTokens)
-            val response = client.post(providerUrl) {
+        val maxTokens = 4096 //todo: configurable
+        val requestJson = JsonObject()
+            .put("model", request.model.id)
+            .put("messages", JsonArray(request.messages.map { it.toJson() }))
+            .put("max_tokens", maxTokens)
+
+        val response = try {
+            client.post(providerUrl) {
                 header("Content-Type", "application/json")
                 header("Accept", "application/json")
                 header("x-api-key", providerKey)
                 header("anthropic-version", "2023-06-01")
                 setBody(requestJson.encode())
             }
-            val roundTripTime = response.responseTime.timestamp - response.requestTime.timestamp
-            log.debug("Anthropic response status: ${response.status} in $roundTripTime ms")
-
-            if (response.status.isSuccess()) {
-                val json = JsonObject(response.bodyAsText())
-                val choice = ChatChoice(
-                    index = 0, //todo: multiple choices
-                    ChatMessage(
-                        ChatRole.Assistant,
-                        TextContent(json.getJsonArray("content").getJsonObject(0).getString("text"))
-                    )
-                )
-                val completion = ChatCompletion(
-                    id = json.getString("id"),
-                    created = System.currentTimeMillis(),
-                    model = ModelId(json.getString("model")),
-                    choices = listOf(choice),
-                    usage = Usage(
-                        promptTokens = json.getJsonObject("usage").getInteger("input_tokens"),
-                        completionTokens = json.getJsonObject("usage").getInteger("output_tokens"),
-                        totalTokens = 0
-                    ).let { it.copy(totalTokens = it.promptTokens!! + it.completionTokens!!) }
-                )
-                return completion
-            } else if (response.status.value == 401) {
-                throw AuthenticationException(
-                    response.status.value,
-                    OpenAIError(
-                        OpenAIErrorDetails(
-                            code = null,
-                            message = "Unauthorized access to Anthropic. Please check your API key and try again.",
-                            param = null,
-                            type = null
-                        )
-                    ),
-                    ClientRequestException(response, response.bodyAsText())
-                )
-            } else if (response.status.value == 429) {
-                var errorMessage = "Rate limit exceeded. Please try again later."
-                try {
-                    val jsonBody = JsonObject(response.bodyAsText())
-                    if (jsonBody.containsKey("error")) {
-                        errorMessage = jsonBody.getJsonObject("error").getString("message")
-                    }
-                } catch (_: Exception) {
-                }
-                throw RateLimitException(
-                    response.status.value,
-                    OpenAIError(
-                        OpenAIErrorDetails(
-                            code = null,
-                            message = errorMessage,
-                            param = null,
-                            type = null
-                        )
-                    ),
-                    ClientRequestException(response, response.bodyAsText())
-                )
-            } else {
-                var errorMessage = "Invalid request to Anthropic. Please check your request and try again."
-                try {
-                    val jsonBody = JsonObject(response.bodyAsText())
-                    if (jsonBody.containsKey("error")) {
-                        errorMessage = jsonBody.getJsonObject("error").getString("message")
-                    }
-                } catch (_: Exception) {
-                }
-
-                log.warn("Anthropic completion failed: ${response.status}. Message: ${response.bodyAsText()}")
-                throw InvalidRequestException(
-                    response.status.value,
-                    OpenAIError(
-                        OpenAIErrorDetails(
-                            code = null,
-                            message = errorMessage,
-                            param = null,
-                            type = null
-                        )
-                    ),
-                    ClientRequestException(response, response.bodyAsText())
-                )
-            }
         } catch (e: HttpRequestTimeoutException) {
             throw OpenAITimeoutException(e)
         }
+        val roundTripTime = response.responseTime.timestamp - response.requestTime.timestamp
+        log.debug("Anthropic response status: ${response.status} in $roundTripTime ms")
+
+        throwIfError(response)
+        val json = JsonObject(response.bodyAsText())
+        val choice = ChatChoice(
+            index = 0, //todo: multiple choices
+            ChatMessage(
+                ChatRole.Assistant,
+                TextContent(json.getJsonArray("content").getJsonObject(0).getString("text"))
+            )
+        )
+        val completion = ChatCompletion(
+            id = json.getString("id"),
+            created = System.currentTimeMillis(),
+            model = ModelId(json.getString("model")),
+            choices = listOf(choice),
+            usage = Usage(
+                promptTokens = json.getJsonObject("usage").getInteger("input_tokens"),
+                completionTokens = json.getJsonObject("usage").getInteger("output_tokens"),
+                totalTokens = 0
+            ).let { it.copy(totalTokens = it.promptTokens!! + it.completionTokens!!) }
+        )
+        return completion
+    }
+
+    private suspend fun throwIfError(response: HttpResponse) {
+        if (response.status.isSuccess()) return
+
+        if (response.status.value == 401) {
+            throw AuthenticationException(
+                response.status.value,
+                OpenAIError(
+                    OpenAIErrorDetails(
+                        code = null,
+                        message = "Unauthorized access to Anthropic. Please check your API key and try again.",
+                        param = null,
+                        type = null
+                    )
+                ),
+                ClientRequestException(response, response.bodyAsText())
+            )
+        } else if (response.status.value == 429) {
+            var errorMessage = "Rate limit exceeded. Please try again later."
+            try {
+                val jsonBody = JsonObject(response.bodyAsText())
+                if (jsonBody.containsKey("error")) {
+                    errorMessage = jsonBody.getJsonObject("error").getString("message")
+                }
+            } catch (_: Exception) {
+            }
+            throw RateLimitException(
+                response.status.value,
+                OpenAIError(
+                    OpenAIErrorDetails(
+                        code = null,
+                        message = errorMessage,
+                        param = null,
+                        type = null
+                    )
+                ),
+                ClientRequestException(response, response.bodyAsText())
+            )
+        }
+
+        var errorMessage = "Invalid request to Anthropic. Please check your request and try again."
+        try {
+            val jsonBody = JsonObject(response.bodyAsText())
+            if (jsonBody.containsKey("error")) {
+                errorMessage = jsonBody.getJsonObject("error").getString("message")
+            }
+        } catch (_: Exception) {
+        }
+
+        val log = project.getVoqalLogger(this::class)
+        log.warn("Anthropic completion failed: ${response.status}. Message: ${response.bodyAsText()}")
+        throw InvalidRequestException(
+            response.status.value,
+            OpenAIError(
+                OpenAIErrorDetails(
+                    code = null,
+                    message = errorMessage,
+                    param = null,
+                    type = null
+                )
+            ),
+            ClientRequestException(response, response.bodyAsText())
+        )
     }
 
     override fun getAvailableModelNames() = MODELS
