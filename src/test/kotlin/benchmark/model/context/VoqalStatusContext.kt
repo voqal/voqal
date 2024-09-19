20efabf024bd2a9065956c254bc4f8b951aa9cfa
Brandon Fergerson <bfergerson@apache.org>
2024-09-19 13:41:43 -0300

    chore(bench): bench by mode

---
+++ src/test/kotlin/benchmark/model/context/VoqalStatusContext.kt
@@ -0,0 +1,6 @@
+package benchmark.model.context
+
+import dev.voqal.assistant.context.VoqalContext
+import dev.voqal.status.VoqalStatus
+
+class VoqalStatusContext(val status: VoqalStatus) : VoqalContext
